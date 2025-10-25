ea7780332b9b7a5a6eda86da7454f23d32d53e9a
gjc14 <yin@yinc.me>
2025-10-26 01:33:33 +0800

    feat(ec/product): submit save and moveToTrash to server

--- app/routes/services/ecommerce/dashboard/02_products/02_product/components/alerts.tsx
+++ app/routes/services/ecommerce/dashboard/02_products/02_product/components/alerts.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react'
+import { useFetcher, useNavigate } from 'react-router'
+
 import { atom, useAtom, useAtomValue } from 'jotai'
 
 import {
@@ -11,9 +14,12 @@ import {
 	AlertDialogTitle,
 	AlertDialogTrigger,
 } from '~/components/ui/alert-dialog'
+import { useFetcherNotification } from '~/hooks/use-notification'
 import { productAtom } from '~/routes/services/ecommerce/store/product/context'
 
+import type { action } from '../../resource'
 import {
+	isMovingToTrashAtom,
 	isResetAlertOpenAtom as isResetOpenAtom,
 	isRestoreAlertOpenAtom as isRestoreOpenAtom,
 	isToTrashAlertOpenAtom as isToTrashOpenAtom,
@@ -23,41 +29,81 @@ const productIdAtom = atom(get => get(productAtom)?.id || null)
 const productNameAtom = atom(get => get(productAtom)?.name || null)
 
 export function ProductAlerts() {
+	const fetcher = useFetcher<typeof action>()
+	const navigate = useNavigate()
 	const productId = useAtomValue(productIdAtom)
 	const productName = useAtomValue(productNameAtom)
+	useFetcherNotification(fetcher)
 
 	const [resetAlertOpen, setResetAlertOpen] = useAtom(isResetOpenAtom)
 	const [restoreAlertOpen, setRestoreAlertOpen] = useAtom(isRestoreOpenAtom)
 	const [toTrashAlertOpen, setToTrashAlertOpen] = useAtom(isToTrashOpenAtom)
+	const [isMovingToTrash, setIsMovingToTrash] = useAtom(isMovingToTrashAtom)
+
+	useEffect(
+		() => setIsMovingToTrash(fetcher.state === 'submitting'),
+		[fetcher.state],
+	)
+
+	useEffect(() => {
+		if (!fetcher.data || 'err' in fetcher.data) return
+		navigate('..')
+	}, [fetcher.data])
 
 	if (!productId || !productName) return null
+
+	const onDelete = () => {
+		if (isMovingToTrash) return
+		fetcher.submit([{ id: productId, name: productName }], {
+			method: 'DELETE',
+			action: `/dashboard/ecommerce/products/resource`,
+			encType: 'application/json',
+		})
+	}
 
 	return (
 		<>
 			<ProductActionAlert
 				title="Reset Product"
-				description={`You're going to discard unsaved changes and reset "${productName}" to latest saved data. This action cannot be undone.`}
+				description={
+					<>
+						You're going to discard unsaved changes and reset{' '}
+						<span className="text-primary font-medium">{productName}</span> to
+						latest saved data. This action cannot be undone.
+					</>
+				}
 				actionTitle="Reset"
-				cancelTitle="Cancel"
 				onAction={() => console.log('Product reset', productId)}
 				open={resetAlertOpen}
 				onOpenChange={setResetAlertOpen}
 			/>
 			<ProductActionAlert
 				title="Unsaved Content Detected"
-				description={`You're going to restore "${productName}" to its last unsaved state. This action will discard current unsaved changes.`}
+				description={
+					<>
+						You're going to restore{' '}
+						<span className="text-primary font-medium">{productName}</span> to
+						its last unsaved state. This action will discard current unsaved
+						changes.
+					</>
+				}
 				actionTitle="Restore"
-				cancelTitle="Cancel"
+				cancelTitle="Discard"
 				onAction={() => console.log('Product restored', productId)}
 				open={restoreAlertOpen}
 				onOpenChange={setRestoreAlertOpen}
 			/>
 			<ProductActionAlert
 				title="Move Product to Trash"
-				description={`You're going to move "${productName}" to the trash. You can restore from Trash within 30 days.`}
-				actionTitle="Delete"
-				cancelTitle="Cancel"
-				onAction={() => console.log('Product to trash', productId)}
+				description={
+					<>
+						You're going to move{' '}
+						<span className="text-primary font-medium">{productName}</span> to
+						the trash. You can restore from Trash within 30 days.
+					</>
+				}
+				actionTitle="Move to Trash"
+				onAction={onDelete}
 				open={toTrashAlertOpen}
 				onOpenChange={setToTrashAlertOpen}
 			/>
@@ -71,7 +117,7 @@ function ProductActionAlert({
 	title,
 	description,
 	actionTitle,
-	cancelTitle,
+	cancelTitle = 'Cancel',
 	onAction,
 	onCancel,
 	children,
@@ -79,9 +125,9 @@ function ProductActionAlert({
 	open: boolean
 	onOpenChange: (open: boolean) => void
 	title: string
-	description?: string
-	actionTitle: string
-	cancelTitle: string
+	description?: React.ReactNode
+	actionTitle?: string
+	cancelTitle?: string
 	onAction: () => void
 	onCancel?: () => void
 	children?: React.ReactNode
