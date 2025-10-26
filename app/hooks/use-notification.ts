3d5b4be94a6b5548d9a502f1b23abbd66d076fe3
gjc14 <yin@yinc.me>
2025-10-27 02:15:16 +0800

    feat(hook): fetcher hook to track fetcher.data for notification

--- app/hooks/use-notification.ts
+++ app/hooks/use-notification.ts
@@ -3,10 +3,7 @@ import { useFetchers, type FetcherWithComponents } from 'react-router'
 
 import { toast, type ExternalToast } from '@gjc14/sonner'
 
-type ServerNotificationResponse = {
-	msg?: string
-	err?: string
-}
+import type { ActionResponse } from '~/lib/utils'
 
 /** Function to check if fetchers have response that satisfies auto notification */
 export function useServerNotification() {
@@ -39,30 +36,72 @@ export function useServerNotification() {
 }
 
 /**
+ * Hook extends fetcher to show notification based on conventional action response {@link ActionResponse}
  *
- * @param fetcher Fetcher that needed notification
- * @param onSuccess
- * @param onError
- * @param options
+ * **IMPORTANT**: When lighting fast requests happen, the state may skip 'loading' or 'submitting' state.
+ *
+ * ```markdown
+ * Data flow:
+ * submitting   --->   loading   --->   idle
+ * ^action()           ^loader()        ^done
+ * ^no data            ^data            ^data
+ * ```
+ *
+ * fetcher.data is available once state is 'loading' when calling with fetcher.submit,
+ * and is available once state is 'idle' when calling with fetcher.load.
  */
-export function useFetcherNotification(
-	fetcher: FetcherWithComponents<ServerNotificationResponse>,
-	onSuccess?: (msg?: string) => void,
-	onError?: (err?: string) => void,
-	options?: {
+export function useFetcherNotification<
+	T extends ActionResponse,
+> /** Fetcher to subscribe, should fetch an action returning {@link ActionResponse} */(
+	fetcher: FetcherWithComponents<T>,
+	props: {
+		/** Callback on success */
+		onSuccess?: (msg?: string) => void
+		/** Callback on error */
+		onError?: (err?: string) => void
+		/** Customize success message instead of returned from server */
 		successMessage?: string
+		/** Customize error message instead of returned from server */
 		errorMessage?: string
-	} & ExternalToast,
+		/** Prevent success alert from showing @default false */
+		preventSuccessAlert?: boolean
+		/** Prevent error alert from showing @default false */
+		preventErrorAlert?: boolean
+	} & ExternalToast = {},
 ) {
+	const {
+		onSuccess,
+		onError,
+		successMessage,
+		errorMessage,
+		preventSuccessAlert = false,
+		preventErrorAlert = false,
+		...toastOptions
+	} = props
+
 	useEffect(() => {
-		if (fetcher.data) {
+		if (fetcher.state === 'idle' && fetcher.data) {
+			const prevented = !!fetcher.data.preventNotification
 			if (fetcher.data.msg) {
-				toast.success(options?.successMessage || fetcher.data.msg, options)
+				if (!prevented || !preventSuccessAlert) {
+					toast.success(successMessage || fetcher.data.msg, toastOptions)
+				}
+
 				onSuccess?.(fetcher.data.msg)
 			} else if (fetcher.data.err) {
-				toast.error(options?.errorMessage || fetcher.data.err, options)
+				if (!prevented && !preventErrorAlert) {
+					toast.error(errorMessage || fetcher.data.err, toastOptions)
+				}
+
 				onError?.(fetcher.data.err)
 			}
 		}
-	}, [fetcher.data])
+	}, [fetcher.state, fetcher.data])
+
+	return {
+		mutating: fetcher.state !== 'idle',
+		isSubmitting: fetcher.state === 'submitting',
+		isLoading: fetcher.state === 'loading',
+		isIdle: fetcher.state === 'idle',
+	}
 }
