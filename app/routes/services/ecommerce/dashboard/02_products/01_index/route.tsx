2da76e5074e5dd2a6a93d4d04128b75f1f939c52
gjc14 <yin@yinc.me>
2025-11-22 23:30:19 +0800

    feat(ec/product): products display using <DashboardDataTable>

--- app/routes/services/ecommerce/dashboard/02_products/01_index/route.tsx
+++ app/routes/services/ecommerce/dashboard/02_products/01_index/route.tsx
@@ -1,6 +1,15 @@
 import type { Route } from './+types/route'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import { Link, useFetcher } from 'react-router'
 
+import { type ColumnDef, type Table } from '@tanstack/react-table'
+import { useAtomValue } from 'jotai'
+
+import { Badge } from '~/components/ui/badge'
+import { DropdownMenuItem } from '~/components/ui/dropdown-menu'
+import { useFetcherNotification } from '~/hooks/use-notification'
+import { DashboardDataTable } from '~/routes/papa/dashboard/components/dashboard-data-table'
+import { useSkipper } from '~/routes/papa/dashboard/components/dashboard-data-table/hooks'
 import {
 	DashboardActions,
 	DashboardContent,
@@ -8,24 +17,28 @@ import {
 	DashboardLayout,
 	DashboardTitle,
 } from '~/routes/papa/dashboard/components/dashboard-wrapper'
-import { DataTable } from '~/routes/papa/dashboard/components/data-table'
+import { DashboardDataTableMoreMenu } from '~/routes/papa/dashboard/components/data-table'
 
 import { getProducts } from '../../../lib/db/product.server'
-import { columns } from './columns'
+import { storeConfigAtom } from '../../../store/product/context'
+import { formatPrice } from '../../../store/product/utils/price'
 
 export const loader = async () => {
 	const products = await getProducts({ relations: true, status: 'ALL' })
 	return { products }
 }
 
-export default function ECProductsIndex({ loaderData }: Route.ComponentProps) {
-	const [rowSelection, setRowSelection] = useState({})
-	const [rowsDeleting, setRowsDeleting] = useState<Set<string>>(new Set())
+type Product = Awaited<ReturnType<typeof getProducts>>[number]
 
-	const tableData = loaderData.products.map(p => ({
-		...p,
-		setRowsDeleting,
-	}))
+export default function ECProductsIndex({ loaderData }: Route.ComponentProps) {
+	const tableRef = useRef<Table<Product>>(null)
+	const [shouldSkip, skip] = useSkipper()
+	const [state, setState] = useState(loaderData.products)
+
+	useEffect(() => {
+		skip()
+		setState(loaderData.products)
+	}, [loaderData])
 
 	return (
 		<DashboardLayout>
@@ -33,21 +46,183 @@ export default function ECProductsIndex({ loaderData }: Route.ComponentProps) {
 				<DashboardTitle title="Products"></DashboardTitle>
 				<DashboardActions>{/* Some action buttons here */}</DashboardActions>
 			</DashboardHeader>
-			<DashboardContent>
-				<DataTable
+			<DashboardContent className="px-0 md:px-0">
+				<DashboardDataTable
 					columns={columns}
-					data={tableData}
-					rowSelection={rowSelection}
-					setRowSelection={setRowSelection}
-					rowGroupStyle={[
-						{
-							rowIds: rowsDeleting,
-							className: 'opacity-50 pointer-events-none',
-						},
-					]}
-					hideColumnFilter
+					data={state}
+					setData={setState}
+					ref={tableRef}
+					autoResetPageIndex={shouldSkip}
+					skipAutoResetPageIndex={skip}
+					className="px-2 md:px-3"
+					initialPageSize={20}
 				/>
 			</DashboardContent>
 		</DashboardLayout>
 	)
 }
+
+export const columns: ColumnDef<Product>[] = [
+	// TODO: stock management
+	{
+		accessorKey: 'id',
+		header: 'Image',
+		cell: ({ row }) => {
+			const slug = row.original.slug
+			const name = row.original.name
+			const image = row.original.option.image
+			return (
+				<div className="flex items-center">
+					<Link
+						to={slug}
+						className="focus-visible:ring-ring/50 m-1 inline-block cursor-pointer hover:underline focus-visible:ring-2 focus-visible:outline-0"
+					>
+						{image ? (
+							<img src={image} alt={name} className="size-12 object-cover" />
+						) : (
+							<p className="flex size-12 items-center justify-center">⛰️</p>
+						)}
+					</Link>
+				</div>
+			)
+		},
+	},
+	{
+		accessorKey: 'name',
+		header: 'Name',
+		cell: ({ row }) => {
+			const slug = row.original.slug
+			const name = row.original.name
+			return (
+				<Link
+					to={slug}
+					className="focus-visible:ring-ring/50 cursor-pointer hover:underline focus-visible:ring-2 focus-visible:outline-0"
+				>
+					{name}
+				</Link>
+			)
+		},
+	},
+	{
+		accessorKey: 'price',
+		header: 'Price',
+		cell: ({ row }) => {
+			const storeConfig = useAtomValue(storeConfigAtom)
+
+			const price = row.original.option.price
+			const salePrice = row.original.option.salePrice
+			const displayPrice = salePrice || price
+			const hasDiscount = !!salePrice && salePrice < price
+
+			const fmt = new Intl.NumberFormat(storeConfig.language, {
+				style: 'currency',
+				currency: row.original.option.currency,
+				// RangeError: maximumFractionDigits value is out of range. Must be between 0 and 100.
+				minimumFractionDigits: row.original.option.scale,
+				maximumFractionDigits: row.original.option.scale,
+			})
+
+			return (
+				<div className="flex flex-col">
+					{fmt.format(
+						formatPrice(
+							displayPrice,
+							row.original.option.scale,
+						) as Intl.StringNumericLiteral,
+					)}
+					{hasDiscount && (
+						<span className="text-muted-foreground text-xs line-through">
+							{fmt.format(
+								formatPrice(
+									price,
+									row.original.option.scale,
+								) as Intl.StringNumericLiteral,
+							)}
+						</span>
+					)}
+				</div>
+			)
+		},
+	},
+	{
+		accessorKey: 'sku',
+		header: 'SKU',
+		cell: ({ row }) => {
+			return row.original.option.sku || '—'
+		},
+	},
+	{
+		accessorKey: 'status',
+		header: 'Status',
+		cell: ({ row }) => {
+			const status = row.original.status
+			let variant: 'default' | 'secondary' | 'destructive' | 'outline' =
+				'default'
+			switch (status) {
+				case 'DRAFT':
+					variant = 'secondary'
+					break
+				case 'PUBLISHED':
+					variant = 'default'
+					break
+				case 'SCHEDULED':
+					variant = 'default'
+					break
+				case 'TRASHED':
+					variant = 'destructive'
+					break
+				case 'ARCHIVED':
+					variant = 'outline'
+					break
+				case 'OTHER':
+					variant = 'outline'
+					break
+				default:
+					break
+			}
+			return (
+				<Badge className="rounded-full" variant={variant}>
+					{status}
+				</Badge>
+			)
+		},
+	},
+	{
+		accessorKey: 'updatedAt',
+		header: 'Updated At',
+		cell: ({ row }) => row.original.updatedAt.toLocaleString('zh-TW'),
+	},
+	{
+		accessorKey: 'id',
+		header: '',
+		cell: ({ row }) => {
+			const fetcher = useFetcher()
+			const { mutating } = useFetcherNotification(fetcher)
+
+			const id = row.original.id
+			const slug = row.original.slug
+			const name = row.original.name
+
+			return (
+				<DashboardDataTableMoreMenu
+					id={id}
+					deletable={true}
+					onDelete={() => {
+						fetcher.submit([{ id, name }], {
+							method: 'DELETE',
+							action: `resource`,
+							encType: 'application/json',
+						})
+					}}
+					permanent={false}
+					deleteTarget={name}
+					mutating={mutating}
+				>
+					<Link to={slug}>
+						<DropdownMenuItem>Edit</DropdownMenuItem>
+					</Link>
+				</DashboardDataTableMoreMenu>
+			)
+		},
+	},
+]
