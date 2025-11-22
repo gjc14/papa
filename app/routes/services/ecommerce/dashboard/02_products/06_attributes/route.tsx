9215119073effa26930fe9fff778596656a88894
gjc14 <yin@yinc.me>
2025-11-22 21:03:40 +0800

    feat(ec/product): display product taxonomies

--- app/routes/services/ecommerce/dashboard/02_products/06_attributes/route.tsx
+++ app/routes/services/ecommerce/dashboard/02_products/06_attributes/route.tsx
@@ -1,5 +1,10 @@
 import type { Route } from './+types/route'
+import { useEffect, useMemo, useRef, useState } from 'react'
 
+import type { ColumnDef, Table } from '@tanstack/react-table'
+
+import { DashboardDataTable } from '~/routes/papa/dashboard/components/dashboard-data-table'
+import { useSkipper } from '~/routes/papa/dashboard/components/dashboard-data-table/hooks'
 import {
 	DashboardActions,
 	DashboardContent,
@@ -9,14 +14,27 @@ import {
 } from '~/routes/papa/dashboard/components/dashboard-wrapper'
 
 import { getEcAttributes } from '../../../lib/db/taxonomy.server'
-import { CreateTaxonomyDialog } from '../../components/create-taxonomy-dialog'
+import { CreateTaxonomyDialog } from '../../components/taxonomy/create-taxonomy-dialog'
 
 export const loader = async ({ request, params }: Route.LoaderArgs) => {
 	const attrs = await getEcAttributes()
 	return { attrs }
 }
 
+export type Attribute = Awaited<ReturnType<typeof getEcAttributes>>[number]
+
 export default function ECAttributes({ loaderData }: Route.ComponentProps) {
+	const tableRef = useRef<Table<Attribute>>(null)
+	const [state, setState] = useState(loaderData.attrs)
+	const [shouldSkip, skip] = useSkipper()
+
+	useEffect(() => {
+		skip()
+		setState(loaderData.attrs)
+	}, [loaderData])
+
+	const columns = useMemo(() => createAttributeColumns(), [])
+
 	return (
 		<DashboardLayout>
 			<DashboardHeader>
@@ -35,12 +53,38 @@ export default function ECAttributes({ loaderData }: Route.ComponentProps) {
 					/>
 				</DashboardActions>
 			</DashboardHeader>
-			{/* Your main content goes here */}
-			<DashboardContent>
-				{loaderData.attrs.map(attr => (
-					<div key={attr.id}>{attr.name}</div>
-				))}
+			<DashboardContent className="px-0 md:px-0">
+				<DashboardDataTable
+					columns={columns}
+					data={state}
+					setData={setState}
+					ref={tableRef}
+					autoResetPageIndex={shouldSkip}
+					skipAutoResetPageIndex={skip}
+					className="px-2 md:px-3"
+					initialPageSize={50}
+				/>
 			</DashboardContent>
 		</DashboardLayout>
 	)
+}
+
+const createAttributeColumns = (): ColumnDef<Attribute>[] => {
+	return [
+		{
+			header: 'Name',
+			footer: props => props.column.id,
+			accessorKey: 'name',
+		},
+		{
+			header: 'Slug',
+			footer: props => props.column.id,
+			accessorKey: 'slug',
+		},
+		{
+			header: 'Value',
+			footer: props => props.column.id,
+			accessorKey: 'value',
+		},
+	]
 }
