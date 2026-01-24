a9843c9375b39d509d8a9c1f1d8e683ac9c2e4f2
gjc14 <yin@yinc.me>
2026-01-24 20:57:32 +0800

    refactor(rrv7): enable middleware

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,10 +1,7 @@
 import type { Config } from '@react-router/dev/config'
 
 export default {
-	// Config options...
 	future: {
-		// unstable_middleware: true,
+		v8_middleware: true,
 	},
-	// Server-side render by default, to enable SPA mode set this to `false`
-	ssr: true,
 } satisfies Config
