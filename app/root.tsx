a4ae1a70347243ae6ce7c14458f98c21de7b06a5
gjc14 <yin@yinc.me>
2026-01-30 19:29:48 +0800

    refactor(ErrorBoundary): replacing web layout ErrorBoundary as root layout

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,21 @@
 import '@gjc14/sonner/dist/styles.css'
 import './app.css'
 
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from 'react-router'
+import {
+	Link,
+	Links,
+	Meta,
+	Outlet,
+	Scripts,
+	ScrollRestoration,
+} from 'react-router'
 
-import { MotionConfig } from 'motion/react'
+import { AlertCircle, ArrowLeft } from 'lucide-react'
+import { motion, MotionConfig } from 'motion/react'
 import { ThemeProvider } from 'next-themes'
+
+import { Button } from '~/components/ui/button'
+import { fade } from '~/components/motions'
 
 import {
 	ErrorBoundaryTemplate,
@@ -100,13 +111,120 @@ export function ErrorBoundary() {
 const ErrorTemplate = ({
 	status,
 	statusMessage,
+	errorMessage,
 }: ErrorBoundaryTemplateProps) => {
+	if (status === 404) {
+		return (
+			<main className="flex h-svh w-screen flex-col items-center justify-center">
+				<title>404 - Page Not Found</title>
+				<meta
+					name="description"
+					content="Sorry, we couldn't find the page you're looking for."
+				/>
+
+				<motion.h1
+					className="mb-2 text-[8rem] leading-none tracking-tight sm:text-[10rem] md:text-[15rem]"
+					{...fade()}
+				>
+					404
+				</motion.h1>
+
+				<h2 className="mb-8 text-2xl font-semibold md:text-3xl">
+					Page Not Found
+				</h2>
+
+				<p className="text-primary/80 mx-3 mb-8 max-w-md text-center text-lg">
+					Sorry, we couldn't find the page you're looking for.
+				</p>
+
+				<Link to={'/'}>
+					<Button variant={'link'}>
+						<ArrowLeft /> Back to Home
+					</Button>
+				</Link>
+			</main>
+		)
+	}
+
+	if (status === 500) {
+		return (
+			<main className="flex h-svh w-screen flex-col items-center justify-center">
+				<title>500 - Internal Server Error</title>
+				<meta
+					name="description"
+					content="Something went wrong on our servers."
+				/>
+
+				<motion.h1
+					className="mb-2 text-[8rem] leading-none tracking-tight sm:text-[10rem] md:text-[15rem]"
+					{...fade()}
+				>
+					500
+				</motion.h1>
+
+				<h2 className="mb-8 text-2xl font-semibold md:text-3xl">
+					Internal Server Error
+				</h2>
+
+				<p className="text-primary/80 mx-3 mb-8 max-w-md text-center text-lg">
+					Something went wrong on our servers.
+				</p>
+
+				<div className="flex flex-col gap-4 sm:flex-row">
+					<Link to={'/'}>
+						<Button variant="outline" className="rounded-full">
+							<ArrowLeft className="h-4 w-4" /> Back to Home
+						</Button>
+					</Link>
+
+					<a href="mailto:contact@ema.il">
+						<Button variant="secondary" className="rounded-full">
+							<AlertCircle />
+							Report this error
+						</Button>
+					</a>
+				</div>
+			</main>
+		)
+	}
+
 	return (
 		<main className="flex h-svh w-screen flex-col items-center justify-center">
-			<p>
-				{status} {statusMessage.text}
-			</p>
-			<p className="text-muted-foreground">{statusMessage.description}</p>
+			<title>
+				{status} - {statusMessage.text}
+			</title>
+			<meta
+				name="description"
+				content={statusMessage.description || 'Route Error Response'}
+			/>
+
+			<a
+				className="z-10 text-sm underline underline-offset-4"
+				href={`https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/${status}`}
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Why am I seeing this error?
+			</a>
+
+			<motion.h1
+				className="mb-2 text-[8rem] leading-none tracking-tight sm:text-[10rem] md:text-[15rem]"
+				{...fade()}
+			>
+				{status}
+			</motion.h1>
+
+			{errorMessage && (
+				<h2 className="mb-8 text-2xl font-semibold md:text-3xl">
+					{errorMessage}
+				</h2>
+			)}
+
+			<Link to={'/'}>
+				<Button variant={'outline'} className="rounded-full">
+					<ArrowLeft /> Back to Home
+				</Button>
+			</Link>
 		</main>
 	)
 }
