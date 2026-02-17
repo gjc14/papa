7b3d02ca1975521a9bf4d3e7d209582dd7413941
gjc14 <cyc@yinc.tw>
2026-02-18 04:01:58 +0800

    refactor(blog): service dashboard config icon

--- app/routes/services/blog/service.dashboard.ts
+++ app/routes/services/blog/service.dashboard.ts
@@ -1,17 +1,26 @@
-import { List, Pen, Plus, Rss } from 'lucide-react'
+import {
+	PenIcon,
+	PlusIcon,
+	RssIcon,
+	TreeStructureIcon,
+} from '@phosphor-icons/react'
 
 import { registerServiceDashboard } from '~/lib/service/dashboard-registry'
 
 registerServiceDashboard({
 	name: 'Blog',
 	description: 'Write and manage your blog posts with ease!',
-	logo: Rss,
+	logo: RssIcon,
 	pathname: '/dashboard/blog',
 	sidebar: {
 		primary: [
-			{ icon: Pen, title: 'Posts', pathname: 'blog' },
-			{ icon: Plus, title: 'Create Post', pathname: 'blog/new' },
-			{ icon: List, title: 'Categories / Tags', pathname: 'blog/taxonomy' },
+			{ icon: PenIcon, title: 'Posts', pathname: 'blog' },
+			{ icon: PlusIcon, title: 'Create Post', pathname: 'blog/new' },
+			{
+				icon: TreeStructureIcon,
+				title: 'Categories / Tags',
+				pathname: 'blog/taxonomy',
+			},
 		],
 	},
 })
